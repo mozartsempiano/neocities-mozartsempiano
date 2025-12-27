4b70e0b9465cdcb0699f4f310aa1373bbfcfba83
mozartsempiano <mozartraulmt@gmail.com>
2025-12-27 00:39:32 -0300

    Refactor CSS and new dream entry

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,60 +11,80 @@ import { setupViewportAnimations } from "/assets/js/viewport-animations.js";
 import { wavyText } from "/assets/js/wavy-text.js";
 
 function isDate(month1to12, day) {
-	const n = new Date();
-	return n.getMonth() + 1 === month1to12 && n.getDate() === day;
+  const n = new Date();
+  return n.getMonth() + 1 === month1to12 && n.getDate() === day;
 }
 
 function areFestiveEffectsEnabled() {
-	return localStorage.getItem("festiveEffects") !== "false";
+  return localStorage.getItem("festiveEffects") !== "false";
 }
 
 // Exportar função para uso externo se necessário
 window.areFestiveEffectsEnabled = areFestiveEffectsEnabled;
 
 document.addEventListener("DOMContentLoaded", () => {
-	import("/assets/js/dither.js").catch((err) => console.error("Erro ao carregar dither.js:", err));
-	carregarNavbar();
-	carregarFooter();
-	toggleCrt();
-	fadeIn();
-	enableTooltips();
-	initHeaderMobile();
-	enableLightMode();
-	initSettingsPanel();
-	// smoothScroll();
-	wavyText();
+  import("/assets/js/dither.js").catch((err) => console.error("Erro ao carregar dither.js:", err));
+  carregarNavbar();
+  carregarFooter();
+  toggleCrt();
+  fadeIn();
+  enableTooltips();
+  initHeaderMobile();
+  enableLightMode();
+  initSettingsPanel();
+  // smoothScroll();
+  wavyText();
 
-	// Inicializa animações de scroll
-	setupViewportAnimations({
-		cascade: true,
-		selector: ".animate-on-scroll", // seletor dos elementos que vão animar
-		animationClass: "fade-in", // animação padrão
-	});
+  // Inicializa animações de scroll
+  setupViewportAnimations({
+    cascade: true,
+    selector: ".animate-on-scroll", // seletor dos elementos que vão animar
+    animationClass: "fade-in", // animação padrão
+  });
 
-	// Verificar se efeitos festivos estão habilitados
-	const festiveEnabled = areFestiveEffectsEnabled();
+  const style = document.createElement("style");
+  style.id = "custom-body-effect";
+  style.textContent = `
+	body::before {
+		content: " ";
+		display: block;
+		position: fixed;
+		inset: 0;
+		background: linear-gradient(rgba(18, 16, 16, 0) 50%, rgba(0, 0, 0, 0.25) 50%),
+			linear-gradient(90deg, rgba(255, 0, 0, 0.06), rgba(0, 255, 0, 0.02), rgba(0, 0, 255, 0.06));
+		background-size: auto, auto;
+		z-index: 99999;
+		background-size: 100% 2px, 3px 100%;
+		pointer-events: none;
+	}
+	`;
+  if (!document.getElementById("custom-body-effect")) {
+    document.head.appendChild(style);
+  }
 
-	if (festiveEnabled) {
-		const now = new Date();
+  // Verificar se efeitos festivos estão habilitados
+  const festiveEnabled = areFestiveEffectsEnabled();
 
-		// Dezembro - Natal
-		if (now.getMonth() === 11) {
-			import("/assets/js/snow.js")
-				.then(({ snowEffect }) => snowEffect())
-				.catch((err) => console.error("Erro ao carregar snow.js:", err));
+  if (festiveEnabled) {
+    const now = new Date();
 
-			document.documentElement.style.setProperty("--clr-main-a0", "var(--clr-green-a0)");
-			document.documentElement.style.setProperty("--clr-main-a10", "var(--clr-green-a10)");
-			document.documentElement.style.setProperty("--clr-main-a20", "var(--clr-green-a20)");
-			document.documentElement.style.setProperty("--clr-main-a30", "var(--clr-green-a30)");
-			document.documentElement.style.setProperty("--clr-main-a40", "var(--clr-green-a40)");
-			document.documentElement.style.setProperty("--clr-main-a50", "var(--clr-green-a50)");
+    // Dezembro - Natal
+    if (now.getMonth() === 11) {
+      import("/assets/js/snow.js")
+        .then(({ snowEffect }) => snowEffect())
+        .catch((err) => console.error("Erro ao carregar snow.js:", err));
 
-			const style = document.createElement("style");
-			style.id = "xmas-style";
-			// Pra deixar as imagens verdes também
-			style.textContent = `
+      document.documentElement.style.setProperty("--clr-main-a0", "var(--clr-green-a0)");
+      document.documentElement.style.setProperty("--clr-main-a10", "var(--clr-green-a10)");
+      document.documentElement.style.setProperty("--clr-main-a20", "var(--clr-green-a20)");
+      document.documentElement.style.setProperty("--clr-main-a30", "var(--clr-green-a30)");
+      document.documentElement.style.setProperty("--clr-main-a40", "var(--clr-green-a40)");
+      document.documentElement.style.setProperty("--clr-main-a50", "var(--clr-green-a50)");
+
+      const style = document.createElement("style");
+      style.id = "xmas-style";
+      // Pra deixar as imagens verdes também
+      style.textContent = `
 	      img.img-clr-main, .novo-icone, div#header-expandida::before {
 	        filter: hue-rotate(180deg) saturate(110%) brightness(90%);
 	      }
@@ -72,34 +92,34 @@ document.addEventListener("DOMContentLoaded", () => {
 	        filter: hue-rotate(180deg) saturate(110%) brightness(110%) contrast(1.3);
 	      }
 	    `;
-			if (!document.getElementById("xmas-style")) {
-				document.head.appendChild(style);
-			}
-		}
+      if (!document.getElementById("xmas-style")) {
+        document.head.appendChild(style);
+      }
+    }
 
-		// Semana do Halloween: 25 a 31 de Outubro
-		if (now.getMonth() === 9 && now.getDate() >= 25 && now.getDate() <= 31) {
-			import("/assets/js/halloween.js")
-				.then(({ halloweenEffect }) => {
-					try {
-						halloweenEffect();
-					} catch (err) {
-						console.error("Erro ao iniciar halloweenEffect:", err);
-					}
-				})
-				.catch((err) => console.error("Erro ao carregar halloween.js:", err));
+    // Semana do Halloween: 25 a 31 de Outubro
+    if (now.getMonth() === 9 && now.getDate() >= 25 && now.getDate() <= 31) {
+      import("/assets/js/halloween.js")
+        .then(({ halloweenEffect }) => {
+          try {
+            halloweenEffect();
+          } catch (err) {
+            console.error("Erro ao iniciar halloweenEffect:", err);
+          }
+        })
+        .catch((err) => console.error("Erro ao carregar halloween.js:", err));
 
-			document.documentElement.style.setProperty("--clr-main-a0", "var(--clr-orange-a0)");
-			document.documentElement.style.setProperty("--clr-main-a10", "var(--clr-orange-a10)");
-			document.documentElement.style.setProperty("--clr-main-a20", "var(--clr-orange-a20)");
-			document.documentElement.style.setProperty("--clr-main-a30", "var(--clr-orange-a30)");
-			document.documentElement.style.setProperty("--clr-main-a40", "var(--clr-orange-a40)");
-			document.documentElement.style.setProperty("--clr-main-a50", "var(--clr-orange-a50)");
+      document.documentElement.style.setProperty("--clr-main-a0", "var(--clr-orange-a0)");
+      document.documentElement.style.setProperty("--clr-main-a10", "var(--clr-orange-a10)");
+      document.documentElement.style.setProperty("--clr-main-a20", "var(--clr-orange-a20)");
+      document.documentElement.style.setProperty("--clr-main-a30", "var(--clr-orange-a30)");
+      document.documentElement.style.setProperty("--clr-main-a40", "var(--clr-orange-a40)");
+      document.documentElement.style.setProperty("--clr-main-a50", "var(--clr-orange-a50)");
 
-			// tonalidade laranja/roxo para alguns elementos
-			const style = document.createElement("style");
-			style.id = "halloween-style";
-			style.textContent = `
+      // tonalidade laranja/roxo para alguns elementos
+      const style = document.createElement("style");
+      style.id = "halloween-style";
+      style.textContent = `
 	      img.img-clr-main, .novo-icone, div#header-expandida::before {
 	        filter: hue-rotate(50deg) saturate(150%) brightness(90%);
 	      }
@@ -107,17 +127,17 @@ document.addEventListener("DOMContentLoaded", () => {
 	        filter: hue-rotate(50deg) saturate(150%) brightness(110%) contrast(1);
 	      }
 	    `;
-			if (!document.getElementById(style.id)) document.head.appendChild(style);
-		}
+      if (!document.getElementById(style.id)) document.head.appendChild(style);
+    }
 
-		// 26 de maio - executa 1 vez por sessão
-		if (isDate(5, 26)) {
-			if (!sessionStorage.getItem("confettiDone")) {
-				import("/assets/js/confetti.js").then((m) => {
-					m.confettiEffect();
-					sessionStorage.setItem("confettiDone", "true");
-				});
-			}
-		}
-	}
+    // 26 de maio - executa 1 vez por sessão
+    if (isDate(5, 26)) {
+      if (!sessionStorage.getItem("confettiDone")) {
+        import("/assets/js/confetti.js").then((m) => {
+          m.confettiEffect();
+          sessionStorage.setItem("confettiDone", "true");
+        });
+      }
+    }
+  }
 });
